726538d4501f7160b1887a32d2daba8473c1b7ac
DayanadGarcia <dayanadiasgarcia@gmail.com>
2025-06-17 16:33:58 -0300

    refact: transaction controller

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 
 @Controller('transfer')
 export class TransactionsController {
@@ -8,14 +17,15 @@ export class TransactionsController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
   async transfer(
     @Req() req: any,
-    @Body() body: { toId: string; amount: number },
-  ) {
+    @Body() body: CreateTransactionDto,
+  ): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     const fromId = req.user.sub;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    await this.transactionsService.transfer(fromId, body.toId, body.amount);
-    return { message: 'Transfer successful' };
+    await this.transactionsService.transfer(fromId, body);
+    return;
   }
 }
