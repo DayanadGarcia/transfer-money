de139d05c1f03f4e157e5a10f62af5f9e02ea206
DayanadGarcia <dayanadiasgarcia@gmail.com>
2025-06-25 17:41:58 -0300

    add transaction entity to typeOrm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { ConfigModule } from '@nestjs/config';
+import { Transaction } from './transactions/entities/transaction.entity';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [User],
+      entities: [User, Transaction],
       synchronize: true,
     }),
     UsersModule,
